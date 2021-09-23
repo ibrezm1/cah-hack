186ca099c407d0982599060f33b865e4e1e5323e
ibrezm1 <59130871+ibrezm1@users.noreply.github.com>
2021-09-23 12:19:53 +0530

    updated-charts

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import TreeMenu, { ItemComponent } from "react-simple-tree-menu";
 
+import { Line, Pie } from "react-chartjs-2";
+
 import "react-simple-tree-menu/dist/main.css";
 
 import "./styles.css";
@@ -92,6 +94,74 @@ function App() {
     ]);
     setOpenNodes(["mammal"]);
   };
+
+  // adding for Charts
+  const chartdata = {
+    labels: [
+      "01/01/2019",
+      "02/01/2019",
+      "03/01/2019",
+      "04/01/2019",
+      "05/01/2019",
+      "06/01/2019",
+      "07/01/2019"
+    ],
+    //backgroundColor: ['rgba(255,0,0,1)'],
+    //lineTension: 1,
+    datasets: [
+      {
+        label: "HSN",
+        fill: false,
+        borderColor: "rgba(255, 0, 0, 0.3)",
+        borderWidth: 1,
+        pointRadius: 2,
+        data: [65, 59, 80, 81, 56, 55, 40]
+      },
+      {
+        label: "CPX",
+        fill: false,
+        borderColor: "rgba(0, 255, 0, 0.3)",
+        borderWidth: 1,
+        pointRadius: 2,
+        data: [70, 32, 45, 65, 87, 92, 99]
+      },
+      {
+        label: "Total",
+        fill: false,
+        borderColor: "blue",
+        borderWidth: 2,
+        pointRadius: 2,
+        data: [135, 91, 125, 144, 143, 143, 139]
+      }
+    ]
+  };
+
+  var chartoptions = {
+    legend: {
+      position: "right",
+      labels: {
+        boxWidth: 10
+      }
+    },
+    scales: {
+      xAxes: [
+        {
+          ticks: { display: false }
+        }
+      ]
+    }
+  };
+
+  const pieData = {
+    labels: ["Red", "Blue", "Yellow"],
+    datasets: [
+      {
+        data: [300, 50, 100],
+        backgroundColor: ["rgba(255,0,0, 1)", "#36A2EB", "#FFCE56"]
+      }
+    ]
+  };
+
   return (
     <div className="App">
       <div
@@ -113,6 +183,17 @@ function App() {
             </ul>
           )}
         </TreeMenu>
+      </div>
+
+      <div className="pc">
+        <div className="w3-container w3-teal">
+          <h1>My Page</h1>
+        </div>
+
+        <div className="App main">
+          <Line data={chartdata} options={chartoptions} />
+          <Pie data={pieData} options={chartoptions} />
+        </div>
       </div>
     </div>
   );
